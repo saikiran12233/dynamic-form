0fe8f6a5d4027c4f770a5bb4c308f71e54fce0be
saikiran12233 <142877686+saikiran12233@users.noreply.github.com>
2024-12-04 09:46:16 +0530

    Assignment

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App'; // Import the App component
-import './index.css'; // Import any global styles if necessary
+import App from './App'; 
+import './index.css'; 
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root') // Ensure it matches the div id in index.html
+  document.getElementById('root') 
 );
